fbea4741124ec3ead51b6a7c81976ddbf48a907f
Nauris Pūķis <hi@pyronaur.com>
2023-05-30 17:51:47 +0300

    Examples as documentation

--- examples/example.ts
+++ examples/example.ts
@@ -1,59 +1,135 @@
 import { Console } from '../src/delivery.console';
 import { logthing } from '../src/index';
 
-const log = logthing('Inception', [
-	'info',
-	'debug',
-	'error',
-	'warn',
-	'no_template',
-	new Console("Inception", "custom", { template: "warn", prefix: 'CUSTOM >> ' }),
-	new Console("Inception", "boom", { prefix: 'Boom 👉', symbol: '💥' }),
+
+/**
+ * Simple example
+ */
+const simple = logthing('Simple');
+// Welcome!
+simple.log("Welcome to logthing!");
+simple.log("By default, you have access to the following methods:", Object.keys(simple));
+// You can also chain methods:
+simple.log("Here's what they look like:\n")
+	.log("Informational message")
+	.debug("Debug message")
+	.warn("Warning message")
+	.error("Error message");
+
+
+/**
+ * How types look like
+ */
+simple.debug("Here's how types look like:")
+	// Newlines
+	.debug("Every comma separated value is placed in a new line", "just", "like", "this")
+	// Primitives
+	.debug("Primitives are displayed as-is:", 1, true, null, undefined)
+	// Objects
+	.debug("Objects are pretty-printed:", { a: 1, b: 2, c: 3 })
+	.debug("Large objects will be split into multiple lines:", {
+		long_long_key1: 1,
+		long_long_key2: 2,
+		long_long_key3: 3,
+		long_long_key4: 4,
+		long_long_key5: 5,
+		long_long_key6: 6,
+	})
+	// Arrays
+	.debug("The same goes for arrays:", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+	.debug("And length arrays like a list of months:", [
+		"January", "February", "March", "April", "May", "June",
+		"July", "August", "September", "October", "November", "December"
+	])
+	// Errors
+	.debug("Errors are displayed with their stack trace:", new Error("This is an error"))
+
+
+/**
+ * Customization
+ */
+const custom = logthing('Customization', [
+	new Console("Customization", "templates", {
+		template: "warn",
+	}),
+
+	// Basic customization
+	new Console("Customization", "about", {
+		symbol: "✪",
+		color: 'cyan',
+	}),
+
+	// Custom template
+	new Console("Customization", "all_in", {
+		color: 'magenta',
+		symbol: "[Symbol]",
+		flag: "[Flag]",
+		prefix: "[Prefix]",
+	}),
+
+
+]);
+
+custom.about(
+	'One of the reasons for building logthing was to have a simple way to customize the output of logs.',
+	`Not everything should be a error or a warning.`,
+	`Sometimes you need to have multiple types of messages, in logthing, they're called "channels".`
+);
+
+custom.templates(
+	`You can use one of the predefined templates:`,
+	Object.keys(Console.templates),
+	"This channel is configured to look like a warning template."
+);
+
+custom.about(
+	`You can also customize the flag, symbol and color of the channel.`,
+	`This channel is configured to have a cyan color, a custom symbol and a custom flag.`,
+	`This is the configuration that was used for "about":`,
+	{
+		symbol: "✪",
+		color: 'cyan',
+	},
+);
+
+custom.all_in(
+	`You can also define a completely custom template.`,
+	`This "all_in" channel has the following options:`,
+	{
+		color: "magenta",
+		symbol: "[Symbol]",
+		flag: "[Flag]",
+		prefix: "[Prefix]",
+	},
+);
+
+
+
+
+custom.about("You can also define your own delivery methods, if you want to ship your logs elsewhere.");
+class LogShipperX<T extends string> {
+	constructor (public readonly name: T) { }
+	deliver(...args: unknown[]) {
+		console.log("*Pretend* writing to FileSystem:", ...args, "\n");
+	}
+	buffer_start() {
+		// not implemented
+	}
+	buffer_end() {
+		// not implemented
+	}
+}
+
+const custom_console = logthing('Custom Console', [
 	[
-		new Console("Inception", "banana", { template: "warn" }),
-		new Console("Inception", "banana", { template: "info" }),
+		new Console("Custom Console", "info"),
+		new LogShipperX("info")
+	],
+	[
+		new Console("Custom Console", "warn"),
+		new LogShipperX("warn")
 	],
 ]);
 
-log
-	.mute("banana")
-	.banana("Banana Template TWICE")
-	.boom("Boom Template")
-	.no_template("No Template")
-	.warn('Warning', 'You are in a dream', 'Dreaming')  // Test logging strings
-	.debug('Welcome', 'You have entered the world of dreams')  // Test logging strings
-	.debug({ character: 'C◍obb' }) // Test logging simple object
-	.debug(['Dream Level 1', 'Dream Level 2'])  // Test logging array
-	.error(new Error('Unexpected dream collapse'))  // Test logging Error objects
-	.info([1, 2, 3, 4, 5, 6])  // Test logging numbers in array
-	.info(3, 2)  // Test logging numbers
-	.debug(() => 'This is a dream function') // Test logging functions
-	.section('BUFFER START INCEPTION')  // Test logging section
-	.info("I'm in inception")
-	.info("I'm in inception")
-	.info("I'm in inception")
-	.info("I'm in inception")
-	.info(true)  // Test logging boolean
-	.debug(undefined) // Test logging undefined
-	.info(null)  // Test logging null
-	.debug(Symbol('Dream'))  // Test logging Symbol
-	.section('DOUBLE INCEPTION')  // Test logging section
-	.debug({   // Test logging multi-line complex object
-		dreamLevel: 'Level 1',
-		events:
-			[{ event: 'Kidnapping Saito' },
-			{ event: 'Hotel zero-gravity fight' },
-			{ event: 'Snow fortress' },
-			{ event: 'Limbo' }],
-		characters: {
-			'Cobb': 'Extractor',
-			'Arthur': 'Point Man',
-			'Mal': 'Shade'
-		},
-		dreamLayers: 4,
-		dreamCollapsed: false
-	})
-	.write()
-	.custom("Custom Template")
-	.boom("Custom Template 2")
-	.banana("🍌 Banana Template")
+custom_console.info("This is an info message that's written to the console and to a custom delivery method.");
+custom_console.warn("This is a warning message that's written to the console and to a custom delivery method.");
